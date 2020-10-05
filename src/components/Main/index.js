330aeeadb7bd5e64ee6d5d08406c982742c65c8c
Mike Nieva <m@mikenieva.com>
2020-10-04 21:26:14 -0500

    Base

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,33 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import Fruit from './Fruit';
-import SearchBox from './SearchBox';
+import React from 'react'
+
 
 export default function Main () {
-    const [data, setData] = useState([])
-    const [filteredData, setFilteredData] = useState([])
-
-    const fetchData = async () => {
-        const res = await fetch('./fruits.json')
-        res.json()
-        .then(data => setData(data))  
-    }
-    
-
-    useEffect(() => {
-        fetchData(); 
-    }, [filteredData])
-
-    const lookForAFruit = (text) => {
-           return setFilteredData(text)
-    }
-
     return (
-        <div id="Contenido">
-            <SearchBox data={data} filterAction={lookForAFruit}/>
-            <div id="Frutas">
-                <Fruit data={filteredData.length !== 0 ? filteredData : data} />
-            </div>
-        </div>
+        <>
+        </>
     )
 }
 
